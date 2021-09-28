b89f37d64cf38f3a0773fdec833d590de1581bcf
ds <ldshoo@naver.com>
2021-09-28 17:48:11 +0900

    initial commit

---
+++ my_pipeline/my_pipeline_stack.py
@@ -0,0 +1,15 @@
+from aws_cdk import core as cdk
+from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
+
+class MyPipelineStack(cdk.Stack):
+
+    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        pipeline =  CodePipeline(self, "Pipeline", 
+                        pipeline_name="MyPipeline",
+                        synth=ShellStep("Synth", 
+                            input=CodePipelineSource.git_hub("OWNER/REPO", "main"),
+                            commands=["npm ci", "npm run build", "npx cdk synth"]
+                        )
+                    )
